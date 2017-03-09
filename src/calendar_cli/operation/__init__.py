d9c5980d650b6274ea5679f352056717319228a1
dloureiro <david.loureiro1@gmail.com>
2017-03-09 16:23:51 +0100

    Adding missing import

--- src/calendar_cli/operation/__init__.py
+++ src/calendar_cli/operation/__init__.py
@@ -2,3 +2,4 @@ from .help_operation import HelpOperation
 from .setup_operation import SetupOperation
 from .summary_operation import SummaryOperation
 from .create_operation import CreateOperation
+from .delete_operation import DeleteOperation
