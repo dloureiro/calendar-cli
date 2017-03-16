024cdde015f2d3ad636b65f225b11d776b2cbc00
dloureiro <david.loureiro1@gmail.com>
2017-03-16 14:32:08 +0100

    a bit of std out

--- src/calendar_cli/setting/setting.py
+++ src/calendar_cli/setting/setting.py
@@ -114,7 +114,8 @@ class Setting(CaseClass):
 
         # decode all args as utf-8
         option, args = arg_parser.parser.parse_args([to_unicode(a, errors='ignore') for a in argv[1:]])
-
+        print("args : ")
+        print("args")
         try:
             if not args:
                 # summary
